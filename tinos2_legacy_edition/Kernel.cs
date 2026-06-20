13bd521a1053bf9c25b1e67eec21b0d8984a8c26
randomusert <127417963+randomusert@users.noreply.github.com>
2026-06-20 12:56:58 +0300

    Bit of delay on boot up, Because why not and I can

--- tinos2_legacy_edition/Kernel.cs
+++ tinos2_legacy_edition/Kernel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using tinos2_legacy_edition.Commands;
 using Sys = Cosmos.System;
+using System.Threading;
 
 namespace tinos2_legacy_edition
 {
@@ -13,8 +14,14 @@ namespace tinos2_legacy_edition
 
         protected override void BeforeRun()
         {
+            Console.Clear();
+            Thread.Sleep(1000);
+            Console.WriteLine("loading Command Manager, Please wait...");
+            Thread.Sleep(1000);
             this.commandManger = new CommandManager();
+            Thread.Sleep(1000);
             Console.WriteLine("Welcome to TINOS 2 Legacy edition");
+            Console.WriteLine("Loaded successfully, type 'help' for a list of commands");
         }
 
         protected override void Run()
